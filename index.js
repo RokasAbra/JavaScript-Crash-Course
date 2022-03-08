8e40512dc4cb4c3f0d1e1b3e91607b20941f518a
RokasAbra <rokasabramavicius@gmail.com>
2022-03-08 22:38:39 +0200

    JavaScript beginner

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword (let if etc...)
+// Should be meaningfull names( not like a1, x, b)
+// Cannot start with a number(1name)
+// Cannot contain a space or hypen (-)
+// Are case-sensitive
+
+let firstName= 'Rokas';
+let lastName= 'Abramavicius';
